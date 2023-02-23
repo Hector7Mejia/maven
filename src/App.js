2cd72781501d34f41ee11847f4e77091387dd6af
Hector Mejia <mejia7hector@gmail.com>
2023-02-22 18:31:49 -0600

    Ticket test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Ticket from './components/Ticket.js';
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Today
         </a>
+        <Ticket></Ticket>
       </header>
     </div>
   );
---
+++ src/components/Ticket.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ticket = props => {
+    return (
+        <React.Fragment>
+            <div>Name</div>
+            <div>Description</div>
+        </React.Fragment>
+    )
+}
+
+export default Ticket;
